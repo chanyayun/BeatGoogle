bc94c1a4fd29404a1a440dd9e59fc159e6897da2
yinhuang119 <40201519+yinhuang119@users.noreply.github.com>
2018-12-13 17:53:36 +0800

    Create WordCounter.java

---
+++ BeatGoogle/src/WordCounter.java
@@ -0,0 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class WordCounter {
+	private String urlStr;
+	private String content;
+
+	public WordCounter(String urlStr) {
+		this.urlStr = urlStr;
+	}
+
+	private String fetchContent() throws IOException {
+		URL url = new URL(this.urlStr);
+		URLConnection conn = url.openConnection();
+		InputStream in = conn.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+		String retVal = "";
+		String line = null;
+
+		while ((line = br.readLine()) != null) {
+			retVal = retVal + line + "\n";
+		}
+
+		return retVal;
+	}
+
+	public int countKeyword(String keyword) throws IOException{
+		if (content == null) {
+			content = fetchContent();
+		}
+
+		content = content.toUpperCase();
+		keyword = keyword.toUpperCase();
+
+		int freq = 0;
+		int num = content.indexOf(keyword, 0);
+		while (num >= 0) {
+			num = content.indexOf(keyword, num + keyword.length());
+			freq++;
+		}
+		return freq;
+	}
+}
