ba1f31e36beb0e50c9ba3f6b171833791d8d74c6
yinhuang119 <40201519+yinhuang119@users.noreply.github.com>
2018-12-13 15:22:21 +0800

    Update Main.java

--- BeatGoogle/src/Main.java
+++ BeatGoogle/src/Main.java
@@ -1,11 +1,36 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+		WebPage rootPage = new WebPage("http://soslab.nccu.edu.tw/Welcome.html", "Welcome");
+		WebTree tree = new WebTree(rootPage);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("almost forgot to update!");
-			System.out.println("yaya");
+		tree.root.addChild(new WebNode(new WebPage("http://soslab.nccu.edu.tw/Welcome.html", "Project")));
+		tree.root.addChild(new WebNode(new WebPage("http://soslab.nccu.edu.tw/Publications.html", "Publication")));
+		tree.root.children.get(1).addChild(new WebNode(
+				new WebPage("https://scholar.google.com/citations?user=IpxUy-YAAAAJ&hl=en", "Google Scholar")));
+		tree.root.children.get(1)
+				.addChild(new WebNode(new WebPage("https://dblp.uni-trier.de/pers/hd/y/Yu:Fang.html", "DBLP")));
+		tree.root.addChild(new WebNode(new WebPage("http://soslab.nccu.edu.tw/Members.html", "Member")));
+		tree.root.addChild(new WebNode(new WebPage("http://soslab.nccu.edu.tw/Courses.html", "Course")));
 
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNextLine()) {
+			int numOfKeywords = sc.nextInt();
+			ArrayList<Keyword> keywords = new ArrayList<>();
+
+			for (int i = 0; i < numOfKeywords; i++) {
+				String name = sc.next();
+				double weight = sc.nextDouble();
+				Keyword k = new Keyword(name, weight);
+				keywords.add(k);
+			}
+
+			tree.setPostOrderScore(keywords);
+			tree.printTree();
+		}
+		sc.close();
 	}
-
 }
