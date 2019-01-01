ca2b96ee76255ca3c8d3cdb9348afca2b8ad5b04
yinhuang119 <40201519+yinhuang119@users.noreply.github.com>
2019-01-01 20:53:22 +0800

    Update WebPage.java

--- BeatGoogle/src/WebPage.java
+++ BeatGoogle/src/WebPage.java
@@ -4,20 +4,33 @@ import java.util.ArrayList;
 public class WebPage {
 	public String url;
 	public String name;
-	public WordCounter counter;
+	public KeywordCounter counter;
 	public double score;
 
 	public WebPage(String url, String name) {
 		this.url = url;
 		this.name = name;
-		this.counter = new WordCounter(url);
+		this.counter = new KeywordCounter(url);
 	}
 
-	public void setScore(ArrayList<Keyword> keywords) throws IOException {
+	public void setScore(ArrayList<Keyword> keywords) {
 		this.score = 0;
 
-		for (Keyword k : keywords) {
-			this.score += counter.countKeyword(k.name) * k.weight;
+		try {
+			for (Keyword k : keywords) {
+				if (name.contains("news")) {
+					this.score += counter.countKeyword(k.name) * k.weight * 5;
+				} else {
+					this.score += counter.countKeyword(k.name) * k.weight;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public String toString() {
+		return name + " [" + score + "]\n" + url + "\n\n";
 	}
 }
