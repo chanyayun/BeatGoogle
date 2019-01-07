4f6c8c8059939da743cc0458493fe17cba7f5185
yinhuang119 <40201519+yinhuang119@users.noreply.github.com>
2019-01-07 20:50:46 +0800

    Update WebNode.java

--- BeatGoogle/src/WebNode.java
+++ BeatGoogle/src/WebNode.java
@@ -6,12 +6,18 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class WebNode {
 	public WebPage webPage;
 	public ArrayList<WebPage> children;
 	public double nodeScore;
 	private String url;
 	public String content;
+	public String subPage = "\tSubPages:\n";
 
 	public WebNode(WebPage webPage) throws IOException {
 		this.webPage = webPage;
@@ -23,12 +29,15 @@ public class WebNode {
 		webPage.setScore(keywords);
 		for (WebPage child : children) {
 			child.setScore(keywords);
-			this.nodeScore += child.score;
+			this.nodeScore += child.score * 0.3;
+			subPage = subPage + "\t" + child.name + " [" + child.score + "]\n\t" + child.url + "\n";
 		}
 		webPage.score += this.nodeScore;
+		if (children.size() < 3) {
+			subPage = subPage + "\t(No fit SubPages)\n";
+		}
 	}
 
-	//addChild用
 	private String fetchContent() throws IOException {
 		URL url = new URL(this.url);
 		URLConnection conn = url.openConnection();
@@ -45,26 +54,29 @@ public class WebNode {
 		return retVal;
 	}
 
-	//抓子網頁
 	public void addChild() throws IOException {
-		children.add(new WebPage("http://www.sportingnews.com/ca/nba/news", "test1"));
-		children.add(new WebPage("http://www.sportingnews.com/ca/nfl/news", "test2"));
-		children.add(new WebPage("http://www.sportingnews.com/ca/nhl/news", "test3"));
-
-//		this.url = "http://www.sportingnews.com/ca/nba/news";	
-//		if (this.content == null) {
-//			this.content = fetchContent();
-//		}
-//		
+//		this.url = webPage.url;
+//		this.content = fetchContent();
+//
 //		Document document = Jsoup.parse(this.content);
-//		Elements lis = document.select("li.media");
+//		Elements lis = document.select("div.g");
 //		for (Element li : lis) {
 //			try {
-//				String citeUrl = "http://www.sportingnews.com" + li.attr("abs:href");
+//				Element cite = li.select("cite").get(0);
+//				String citeUrl = cite.text();
 //				Element h3 = li.select("h3.r").get(0);
 //				String title = h3.text();
-//
-//				web.add(new WebPage(citeUrl, title));
+		for (int i = 0; i < 3; i++) {
+			String citeUrl = "https://www.foxnews.com/category/sports/nba.html";
+			String title = "sub";
+
+				if (citeUrl.length() > 40) {
+					children.add(new WebPage(citeUrl, title));
+				}
+				if (children.size() >= 3) {
+					break;
+				}
+		}
 //			} catch (IndexOutOfBoundsException e) {
 //			}
 //		}
